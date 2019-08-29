6c993ff02a030317bdcaf00dfd4eb47d432f1e9b
bishal <bishalmukherjee7@gmail.com>
2019-08-29 20:20:37 +0530

    Buttons are added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+    
     def index
     end
+
+    def download_pdf
+        render 'index'
+    end
+
+    def download_csv
+        render 'index'
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'home/download/pdf', to: 'home#download_pdf'
+  get 'home/download/csv', to: 'home#download_csv'
 end
